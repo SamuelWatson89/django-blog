093f8c59c940dd0de844e3001c75163198ae6db1
Samuel Watson <Sam@Sams-iMac.local>
2019-09-16 16:11:19 +0100

    Added localhost to allowed IP for dev

--- blog/settings.py
+++ blog/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sams-dango-blog.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'sams-dango-blog.herokuapp.com']
 
 
 # Application definition
